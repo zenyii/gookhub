b92bbaa7b26e110b8796085d242fb7410e85181c
zenyii <1048677004@qq.com>
2019-11-14 18:26:48 +0800

    待解决跨域问题

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 let http = axios.create({
-    baseURL: '/api',
+    baseURL: 'http://api.gookhub.cn',
     withCredentials: true,
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
